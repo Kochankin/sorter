267fe56e1fa26097e1bc1ebb8398aa1ff7a7e604
Marianna <31858221+Kochankin@users.noreply.github.com>
2018-02-26 14:39:11 +0300

    attempt2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 class Sorter {
   constructor() { 
-   this.comparator = false;
-   this.elementStorage = [];
+    this.elementStorage = [];
+		this.compareFunction = function(a, b) {
+        return a - b;
+     };
   }
 
   add(element) { 
@@ -21,42 +23,24 @@ class Sorter {
   } 
 
   sort(indices) { 
-    // в отдельный массив клады те элементы, что нужно отсортировать
+		
    var elementsToSort = [];
     for (var i = 0; i < indices.length; i++){
       elementsToSort.push(this.elementStorage[indices[i]]);
     }  
-
-    // из основного массива убираю эти элементы
-    this.elementStorage = this.elementStorage.filter(function(item, i){
-      return indices.indexOf(i) === -1; 
-    });
-
-    // если setComparator === false, то по умолчанию сортирую, если нет - значит был вызван setComparator и надо использовать заданную функцию для сортировки
-    if (this.comparator === false) {  
-     elementsToSort.sort(function(a, b) {
-        return a - b;
-     });
-    } else {
-      elementsToSort.sort(this.compareFunction);
-      this.comparator === false; // убираю флаг
-    }
- 
-    // если все элементы попали в сортировку, возвращаю массив из хранилища
-    if (this.elementStorage.length === 0) {
-        this.elementStorage = elementsToSort; 
-    // если не все - соединяю отсортированную часть с оставшимся массивом
-    } else {
-      this.elementStorage = elementsToSort.concat(this.elementStorage); 
-    }
+		
+		elementsToSort.sort(this.compareFunction);
+		indices.sort(this.compareFunction);
+		
+		for (var i = 0; i < indices.length; i++){
+			this.elementStorage[indices[i]] = elementsToSort[i];
+			}
 
      return this.elementStorage;
   }
 
   setComparator(compareFunction) {
-    // ставлю флаг  и запоминаю введенную функцию
     this.compareFunction = compareFunction; 
-    return this.comparator = true;
   }
 }
 
