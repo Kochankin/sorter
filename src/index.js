f5963e2e2a7a4ef14df96bb9074335f9343c4b4f
Marianna <31858221+Kochankin@users.noreply.github.com>
2018-02-26 15:00:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,49 @@
 class Sorter {
-  constructor() { 
-    this.elementStorage = [];
-		this.compareFunction = function(a, b) {
-        return a - b;
-     };
-  }
+    constructor() {
+        this.elementStorage = [];
+        this.defaultComparator = function (a, b) {
+            return a - b;
+        };
+        this.compareFunction = this.defaultComparator;
+    }
 
-  add(element) { 
-    return this.elementStorage.push(element);
-  }
+    add(element) {
+        return this.elementStorage.push(element);
+    }
 
-  at(index) {
-     return this.elementStorage[index];
-  }
+    at(index) {
+        return this.elementStorage[index];
+    }
 
-  get length() {
-    return this.elementStorage.length;
-  }
+    get length() {
+        return this.elementStorage.length;
+    }
 
-  toArray() {
-   return this.elementStorage;
-  } 
+    toArray() {
+        return this.elementStorage;
+    }
 
-  sort(indices) { 
-		
-   var elementsToSort = [];
-    for (var i = 0; i < indices.length; i++){
-      elementsToSort.push(this.elementStorage[indices[i]]);
-    }  
-		
-		elementsToSort.sort(this.compareFunction);
-		indices.sort(this.compareFunction);
-		
-		for (var i = 0; i < indices.length; i++){
-			this.elementStorage[indices[i]] = elementsToSort[i];
-			}
+    sort(indices) {
 
-     return this.elementStorage;
-  }
+        var elementsToSort = [];
+        for (var i = 0; i < indices.length; i++) {
+            elementsToSort.push(this.elementStorage[indices[i]]);
+        }
 
-  setComparator(compareFunction) {
-    this.compareFunction = compareFunction; 
-  }
+        elementsToSort.sort(this.compareFunction);
+        indices.sort(this.defaultComparator);
+        console.log(this.compareFunction);
+
+        for (var i = 0; i < indices.length; i++) {
+            this.elementStorage[indices[i]] = elementsToSort[i];
+        }
+
+        return this.elementStorage;
+    }
+
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
